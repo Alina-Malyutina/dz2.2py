492e4c708ec6c4ff32c5255dad4447598932ebbd
Alina Malyutina <linamalutina1@gmail.com>
2023-09-27 21:02:50 +0300

    edit first

--- task1.py
+++ task1.py
@@ -3,9 +3,11 @@ number = int(input("Enter your number. It must be positive and more than 0: "))
 hexadecimal_digits = "0123456789ABCDEF"
 SYS_16 = 16
 hexadecimal_number = []
-
-while number > 0:
-    hexadecimal_number.insert(0, hexadecimal_digits[int(number % SYS_16)])
-    number //= SYS_16
+input_num = number
+while input_num > 0:
+    hexadecimal_number.insert(0, hexadecimal_digits[int(input_num % SYS_16)])
+    input_num //= SYS_16
 
 print("Your number in hexadecimal system is", *hexadecimal_number)
+
+print("Check by function:", hex(number))
